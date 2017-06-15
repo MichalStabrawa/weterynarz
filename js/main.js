e39f2e6b414655e679794b8b437f6f8828232b24
Michal Stabrawa <mistaaqua@gmail.com>
2017-06-15 16:36:05 +0200

    section drive
    google maps

--- js/main.js
+++ js/main.js
@@ -1,32 +1,32 @@
 $(function () {
-    var okno = $(window);
-    var menu = $('#main-nav');
+  var okno = $(window);
+  var menu = $('#main-nav');
 
-   if (okno.scrollTop() >= 100) {
+  if (okno.scrollTop() >= 100) {
       menu.addClass('scroll');
    }
 
    okno.scroll(function () {
 
-      if (okno.scrollTop() >= 100) {
-            menu.addClass('scroll');
-       } else {
-            menu.removeClass('scroll');
-        }
+     if (okno.scrollTop() >= 100) {
+           menu.addClass('scroll');
+      } else {
+           menu.removeClass('scroll');
+       }
   });
 
-    /*Smoth scroll*/
+  /*Smoth scroll*/
 
-    $(document).on('click', 'a[href^="#"]', function(event) {
+   $(document).on('click', 'a[href^="#"]', function(event) {
        event.preventDefault();
 var menuHeight = $('#main-nav').height();
-       $('html, body').animate({
+     $('html, body').animate({
             scrollTop: $($.attr(this, 'href')).offset().top - menuHeight
-        }, 500);
-  });
+       }, 500);
+ });
 
 
-});
+}); 
 
 $(document).ready(function(){
     $("button").click(function(){
